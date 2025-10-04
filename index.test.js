17e3234f2fb70552907c38e40f262e272dd2c7ad
prajapai-satyam <satyamprajapati1979@gmail.com>
2025-10-04 10:01:45 +0530

    test pass

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ describe("Express App", () => {
     it("Test / GET endpoint", async()=> {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe("Hello World");
+        expect(res.text).toBe("Hello World!");
     })
     it("Test / :id params GET endpoint", async() =>  {
       const res = await request(app).get('/12');
